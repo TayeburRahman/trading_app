9600c5f770e0e2dfff4900798464dc3b5e09e43a
TayeburRahman <tayebrayhan101+@gmail.com>
2025-01-10 16:22:04 +0600

    update 5

--- src/app/modules/messages/message.service.ts
+++ src/app/modules/messages/message.service.ts
@@ -109,67 +109,71 @@ const sendMessage = async (senderId: any, socket: Socket, io: Server): Promise<v
   });
 };
 
-const sendMessageOne = async (req: any): Promise<void> => { 
-    try {
-     const {userId: senderId} = req.user;
-     const files = req.files;
-      const { receiverId, message, } = req.body as any; 
+const sendMessageOne = async (req: any)=> {
+  try {
+    const { userId: senderId } = req.user;
+    const files = req.files;
+    const { receiverId, message } = req.body as any;
 
- 
- 
-      if (!receiverId || !senderId) {
-        throw new ApiError(404, 'Sender or Receiver user not found');
-      }
- 
-      let conversation = await Conversation.findOne({
-        participants: { $all: [senderId, receiverId] },
+    console.log("========", receiverId, message)
+
+    if (!receiverId || !senderId) {
+      throw new ApiError(404, 'Sender or Receiver user not found');
+    }
+
+    let conversation = await Conversation.findOne({
+      participants: { $all: [senderId, receiverId] },
+    });
+
+    if (!conversation) {
+      conversation = await Conversation.create({
+        participants: [senderId, receiverId],
       });
+    }
 
-      if (!conversation) {
-        conversation = await Conversation.create({
-          participants: [senderId, receiverId],
-        });
-      }
- 
-      let message_img = '';
-      if (files?.message_img && files.message_img.length > 0) {
-        message_img = `/images/message/${files.message_img[0].filename}`;
-      }
- 
- 
-      const newMessage = new Message({
-        senderId,
-        receiverId,
-        message,
-        message_img,
-        conversationId: conversation._id,
-      });  
- 
-      conversation.messages.push(newMessage._id);
- 
-      await Promise.all([conversation.save(), newMessage.save()]);
-       
-      const dbReceiver = await User.findById(receiverId)
-      if(dbReceiver?.deviceToken){
-        const payload = {
-          title: `${dbReceiver.name} Send New Message.`,
-          body: `${message}`
-        };
-        
-        sendPushNotification({ fcmToken: dbReceiver?.deviceToken, payload });
-      }
+    let message_img = '';
+    if (files?.message_img?.length > 0) {
+      message_img = `/images/message/${files.message_img[0].filename}`;
+    }
 
-       //@ts-ignore
-      const socketIo = global.io;
-      if (socketIo && conversation && newMessage) {
-        // console.log("dfsklmjkvfdngvbrfgb", newMessage)
-        socketIo.to(receiverId.toString()).emit('new-message', newMessage);
-        socketIo.to(senderId.toString()).emit('new-message', newMessage);
-      }
-    } catch (error: any) {
-      console.error('Error sending message:', error); 
-    } 
-}; 
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      message,
+      message_img,
+      conversationId: conversation._id,
+    });
+
+    conversation.messages.push(newMessage._id);
+
+    await Promise.all([conversation.save(), newMessage.save()]);
+
+    const dbReceiver = await User.findById(receiverId);
+
+    if (dbReceiver?.deviceToken) {
+      const payload = {
+        title: `${dbReceiver.name} sent a new message.`,
+        body: `${message}`,
+      };
+      sendPushNotification({ fcmToken: dbReceiver?.deviceToken, payload });
+    }
+     
+    //@ts-ignore
+    const socketIo = global.io;
+    if (socketIo) {
+      socketIo.to(receiverId.toString()).emit('new-message', newMessage);
+      socketIo.to(senderId.toString()).emit('new-message', newMessage);
+    }
+
+    console.log('======', newMessage)
+
+     return { message: 'Message sent successfully' };
+
+  } catch (error: any) {
+    console.error('Error sending message:', error?.message || error);
+  }
+};
+
 
 const getMessages = async (req: Request, res: Response) => {
   try {
