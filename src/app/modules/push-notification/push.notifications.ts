ffe8cbd48423deb9513a3846397e102d85ecfde4
TayeburRahman <tayebrayhan101+@gmail.com>
2025-01-10 15:22:10 +0600

    push notification

--- src/app/modules/push-notification/push.notifications.ts
+++ src/app/modules/push-notification/push.notifications.ts
@@ -1,11 +1,13 @@
 import admin from 'firebase-admin';
-import path from 'path';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
-// Initialize Firebase Admin SDK
 admin.initializeApp({
-  credential: admin.credential.cert(
-    path.resolve(__dirname, './firebase.config.update.json')
-  ),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  }),
 });
 
 interface NotificationPayload {
@@ -24,7 +26,6 @@ async function sendPushNotification({
   fcmToken,
   payload,
 }: SendNotificationInput): Promise<void> {
-
   console.log('Send notification:', fcmToken, payload);
 
   if (!fcmToken) {
@@ -36,15 +37,13 @@ async function sendPushNotification({
     notification: {
       title: payload.title,
       body: payload.body,
-      // ...(payload.imageUrl ? { imageUrl: payload.imageUrl } : {}),
     },
     data: payload.data || {},
-    token: fcmToken,  // Single token for sending to one device
+    token: fcmToken,
   };
 
   try {
     const response = await admin.messaging().send(message);
-
     console.log('Successfully sent notification:', response);
   } catch (error) {
     console.error('Error sending push notification:', error);
