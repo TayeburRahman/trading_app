b166afe4e1ac1c9d6902adac02bfc1852114e477
manik <maniksarker652@gmail.com>
2025-01-19 14:56:37 +0600

    issue fixed in update small banner function

--- src/app/modules/media/media.service.ts
+++ src/app/modules/media/media.service.ts
@@ -148,6 +148,11 @@ const deleteVideoAdds = async (id: string) => {
 };
 const addSmallBanner = async (req: any) => {
   const files = req.files;
+  if (files.files[0].mimetype.startsWith('image')) {
+    req.body.type = 'image';
+  } else if (files.files[0].mimetype.startsWith('video')) {
+    req.body.type = 'video';
+  }
   const payload = req.body;
 
   if (!files?.files) {
@@ -158,12 +163,19 @@ const addSmallBanner = async (req: any) => {
     payload.files = `/images/banner/${files.files[0].filename}`;
   }
 
-  return await SmallBanner.create(payload);
+  const result = await SmallBanner.create(payload);
+  console.log('result', result);
+  return result;
 };
 
 const updateSmallBanner = async (req: Request) => {
   const { files } = req as any;
   const id = req.params.id;
+  if (files.files[0].mimetype.startsWith('image')) {
+    req.body.type = 'image';
+  } else if (files.files[0].mimetype.startsWith('video')) {
+    req.body.type = 'video';
+  }
   const { ...AddsData } = req.body;
 
   // Validate the ID
