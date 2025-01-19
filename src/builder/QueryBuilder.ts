d820834615d094ce286727f07e3b684e43af89a6
TayeburRahman <tayebrayhan101+@gmail.com>
2025-01-19 17:37:02 +0600

    update all

--- src/builder/QueryBuilder.ts
+++ src/builder/QueryBuilder.ts
@@ -10,69 +10,66 @@ class QueryBuilder<T> {
   }
 
   search(searchableFields: string[]) {
-    const searchTerm = this?.query?.searchTerm;
+    const searchTerm = this.query?.searchTerm as string;
     if (searchTerm) {
       this.modelQuery = this.modelQuery.find({
         $or: searchableFields.map(
           field =>
             ({
               [field]: { $regex: searchTerm, $options: 'i' },
-            }) as FilterQuery<T>,
+            }) as FilterQuery<T>
         ),
       });
     }
-
     return this;
   }
 
   filter() {
-    const queryObj = { ...this.query }; // copy
-
-    // Filtering
-    const excludeFields = ['searchTerm', 'sort', 'limit', 'page', 'fields', 'removeUserId'];
-
+    const queryObj = { ...this.query }; // Create a shallow copy of the query
+    const excludeFields = [
+      'searchTerm',
+      'sort',
+      'limit',
+      'page',
+      'fields',
+      'removeUserId',
+    ];
     excludeFields.forEach(el => delete queryObj[el]);
 
     if (this.query.removeUserId) {
       queryObj.user = { $ne: this.query.removeUserId };
     }
 
-
     this.modelQuery = this.modelQuery.find(queryObj as FilterQuery<T>);
-
     return this;
   }
 
   sort() {
-    const sort =
-      (this?.query?.sort as string)?.split(',')?.join(' ') || '-createdAt';
-    this.modelQuery = this.modelQuery.sort(sort as string);
-
+    const sortField = (this.query?.sort as string) || '-createdAt';
+    this.modelQuery = this.modelQuery.sort(sortField.split(',').join(' '));
     return this;
   }
 
   paginate() {
-    const page = Number(this?.query?.page) || 1;
-    const limit = Number(this?.query?.limit) || 8;
+    const page = Math.max(1, Number(this.query?.page) || 1);
+    const limit = Math.max(1, Number(this.query?.limit) || 8);
     const skip = (page - 1) * limit;
 
     this.modelQuery = this.modelQuery.skip(skip).limit(limit);
-
     return this;
   }
 
   fields() {
-    const fields =
-      (this?.query?.fields as string)?.split(',')?.join(' ') || '-__v';
-
+    const fields = (this.query?.fields as string)?.split(',').join(' ') || '-__v';
     this.modelQuery = this.modelQuery.select(fields);
     return this;
   }
+
   async countTotal() {
     const totalQueries = this.modelQuery.getFilter();
     const total = await this.modelQuery.model.countDocuments(totalQueries);
-    const page = Number(this?.query?.page) || 1;
-    const limit = Number(this?.query?.limit) || 8;
+    const page = Math.max(1, Number(this.query?.page) || 1);
+    const limit = Math.max(1, Number(this.query?.limit) || 8);
     const totalPage = Math.ceil(total / limit);
 
     return {
