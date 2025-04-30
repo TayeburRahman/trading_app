b623ad996c1ddfda954c0fd3f37ca259776ddad0
TayeburRahman <tayebrayhan101+@gmail.com>
2025-04-30 16:29:02 +0600

    updates all 1

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -8,7 +8,8 @@ import ApiError from '../errors/ApiError';
 
 const sendEmail = async (options: IEmailOptions): Promise<void> => {
   const transporter: Transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: config.smtp.smtp_host,
+    port: parseInt(config.smtp.smtp_port as string),
     auth: {
       user: config.smtp.smtp_mail,
       pass: config.smtp.smtp_password,
@@ -34,12 +35,45 @@ const sendEmail = async (options: IEmailOptions): Promise<void> => {
   } catch (err: any) {
     errorLogger.error('❌ SMTP Error:', err.message);
 
-    // if (err.code === 'ETIMEDOUT' || err.message.includes('timeout')) {
-    //   errorLogger.error('SMTP timeout. Restarting server...');
-    //   process.exit(0);
-    // }
+    if (err.code === 'ETIMEDOUT' || err.message.includes('timeout')) {
+      errorLogger.error('SMTP timeout. Restarting server...');
+      process.exit(1);
+    }
     throw new ApiError(400, `${err.message}`)
   }
 };
 
 export default sendEmail;
+
+// import Mailgun from 'mailgun.js';
+// import formData from 'form-data';
+// import config from '../config';
+// import { IEmailOptions } from '../app/modules/auth/auth.interface';
+// import { errorLogger } from '../shared/logger';
+// import ApiError from '../errors/ApiError';
+
+// const mailgun = new Mailgun(formData);
+// const mg = mailgun.client({
+//   username: 'api',
+//   key: config.mailgun.api_key as string,
+// });
+
+// const sendEmail = async (options: IEmailOptions): Promise<void> => {
+//   const { email, subject, html } = options;
+
+//   try {
+//     const data = await mg.messages.create("sandbox113feaa647634dac9d135ef658eefcfc.mailgun.org", {
+//       from: "Mailgun Sandbox <postmaster@sandbox113feaa647634dac9d135ef658eefcfc.mailgun.org>",
+//       to: ["Jorge Dejesus <tayebrayhan101@gmail.com>"],
+//       subject: "Hello Jorge Dejesus",
+//       text: "Congratulations Jorge Dejesus, you just sent an email with Mailgun! You are truly awesome!",
+//     })
+
+//     console.log('✅ Email sent via Mailgun:', data.id);
+//   } catch (err: any) {
+//     errorLogger.error('❌ Mailgun API Error:', err.message);
+//     throw new ApiError(400, `Mailgun error: ${err.message}`);
+//   }
+// };
+
+// export default sendEmail;
